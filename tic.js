ddb9e49b5fc71a822fb7d907a37f4e9c865b742c
Andrew Schmidt <lazerandy01@gmail.com>
2023-09-06 11:50:02 -0400

    Simplified the win validation

--- tic.js
+++ tic.js
@@ -1,11 +1,17 @@
 // Flag tracks which players turn it is
-flag = 1;
+let flag = 1;
 
 // Turn tracks how many spaces have been taken up, 0-8
-turn = 0;
+let turn = 0;
 
 // Check for wins
-victory = false;
+let victory = false;
+
+// Const for valid win combinitaions
+const validWins =  [[0, 1, 2], [3, 4, 5], 
+					[6, 7, 8], [0, 3, 6], 
+					[1, 4, 7], [2, 5, 8], 
+					[0, 4, 8], [2, 4, 6]];
 
 // return true if win, else false
 function checkForWin(boxes, btns) {
@@ -52,38 +58,13 @@ function main() {
 	btn.push(document.getElementById("b8"));
 	btn.push(document.getElementById("b9"));
 
-
-	if (!victory) {
-		checkForWin([b[0], b[1], b[2]], [btn[0], btn[1], btn[2]]);
-	}
-	if (!victory) {
-		checkForWin([b[3], b[4], b[5]], [btn[3], btn[4], btn[5]]);
-	}
-	if (!victory) {
-		checkForWin([b[6], b[7], b[8]], [btn[6], btn[7], btn[8]]);
-	}
-	if (!victory) {
-		checkForWin([b[0], b[3], b[6]], [btn[0], btn[3], btn[6]]);
-	}
-	if (!victory) {
-		checkForWin([b[1], b[4], b[7]], [btn[1], btn[4], btn[7]]);
-	}
-	if (!victory) {
-		checkForWin([b[2], b[5], b[8]], [btn[2], btn[5], btn[8]]);
-	}
-	if (!victory) {
-		checkForWin([b[0], b[4], b[8]], [btn[0], btn[4], btn[8]]);
-	}
-	if (!victory) {
-		checkForWin([b[2], b[4], b[6]], [btn[2], btn[4], btn[6]]);
-	}
-	if (victory) {
-		for (let x of btn) {
-			x.disabled = true;
-		}
+	for (let x of validWins) {
+		checkForWin([b[x[0]], b[x[1]], b[x[2]]], [btn[x[0]], btn[x[1]], btn[x[2]]]);
 	}
 
-	if (!victory) {
+	if (victory)  {
+		disableBoard(btn);
+	} else {
 		if (turn >= 8) {
 			//Tie if all boxes are full
 			document.getElementById('print').innerHTML = "Match Tie";
@@ -105,129 +86,24 @@ function main() {
 // Function to reset game
 function resetBoard() {
 	location.reload();
-	b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = '';
-
-	victory = false;
-	turn = 0;
-	b = [];
-	btn = [];
 }
 
-// Here onwards, functions check turn of the player
-// and put accordingly value X or 0
-function myfunc_3() {
-	if (flag == 1) {
-		document.getElementById("b1").value = "X";
-		document.getElementById("b1").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b1").value = "0";
-		document.getElementById("b1").disabled = true;
-		flag = 1;
+// Function to disable the board
+function disableBoard(btn) {
+	for (let x of btn) {
+		x.disabled = true;
 	}
 }
 
-function myfunc_4() {
+// Mark space function
+function markSpace(space) {
 	if (flag == 1) {
-		document.getElementById("b2").value = "X";
-		document.getElementById("b2").disabled = true;
+		document.getElementById(space).value = "X";
 		flag = 0;
 	}
 	else {
-		document.getElementById("b2").value = "0";
-		document.getElementById("b2").disabled = true;
+		document.getElementById(space).value = "0";
 		flag = 1;
 	}
-}
-
-function myfunc_5() {
-	if (flag == 1) {
-		document.getElementById("b3").value = "X";
-		document.getElementById("b3").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b3").value = "0";
-		document.getElementById("b3").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_6() {
-	if (flag == 1) {
-		document.getElementById("b4").value = "X";
-		document.getElementById("b4").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b4").value = "0";
-		document.getElementById("b4").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_7() {
-	if (flag == 1) {
-		document.getElementById("b5").value = "X";
-		document.getElementById("b5").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b5").value = "0";
-		document.getElementById("b5").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_8() {
-	if (flag == 1) {
-		document.getElementById("b6").value = "X";
-		document.getElementById("b6").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b6").value = "0";
-		document.getElementById("b6").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_9() {
-	if (flag == 1) {
-		document.getElementById("b7").value = "X";
-		document.getElementById("b7").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b7").value = "0";
-		document.getElementById("b7").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_10() {
-	if (flag == 1) {
-		document.getElementById("b8").value = "X";
-		document.getElementById("b8").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b8").value = "0";
-		document.getElementById("b8").disabled = true;
-		flag = 1;
-	}
-}
-
-function myfunc_11() {
-	if (flag == 1) {
-		document.getElementById("b9").value = "X";
-		document.getElementById("b9").disabled = true;
-		flag = 0;
-	}
-	else {
-		document.getElementById("b9").value = "0";
-		document.getElementById("b9").disabled = true;
-		flag = 1;
-	}
+	document.getElementById(space).disabled = true;
 }
