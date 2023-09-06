b18e8cd525aa62662b27f077764b7c72c3f4a71e
Andrew Schmidt <lazerandy01@gmail.com>
2023-09-06 11:23:40 -0400

    Fixed Tie Game Edge Cases

--- tic.js
+++ tic.js
@@ -9,16 +9,19 @@ victory = false;
 
 // return true if win, else false
 function checkForWin(boxes, btns) {
+	// Ensure that all boxes atleast have an X or O. 
 	for (let x of boxes) {
 		if (x === '') return false;
 	}
-	if (boxes[0] === boxes[1] && boxes[1] === boxes [2]) {
+	// Check that all boxes in line are the same value (all x or all o)
+	if (boxes[0] === boxes[1] && boxes[1] === boxes[2]) {
 		victory = true;
 		if (flag === 0) {
 			document.getElementById('print').innerHTML = "Player X won";
 		} else {
 			document.getElementById('print').innerHTML = "Player O won";
 		}
+		// Mark the winning boxes in red
 		btns[0].style.color = "red";
 		btns[1].style.color = "red";
 		btns[2].style.color = "red";
@@ -27,77 +30,77 @@ function checkForWin(boxes, btns) {
 }
 
 function main() {
-	if (turn >= 8) {
-		//Tie
-		document.getElementById('print').innerHTML = "Match Tie";
-	} else {
-		turn++;
-		var b = [];
-		var btn = [];
-		b[0] = document.getElementById('b1').value;
-		b[1] = document.getElementById('b2').value;
-		b[2] = document.getElementById('b3').value;
-		b[3] = document.getElementById('b4').value;
-		b[4] = document.getElementById('b5').value;
-		b[5] = document.getElementById('b6').value;
-		b[6] = document.getElementById('b7').value;
-		b[7] = document.getElementById('b8').value;
-		b[8] = document.getElementById('b9').value;
+	var b = [];
+	var btn = [];
+	b[0] = document.getElementById('b1').value;
+	b[1] = document.getElementById('b2').value;
+	b[2] = document.getElementById('b3').value;
+	b[3] = document.getElementById('b4').value;
+	b[4] = document.getElementById('b5').value;
+	b[5] = document.getElementById('b6').value;
+	b[6] = document.getElementById('b7').value;
+	b[7] = document.getElementById('b8').value;
+	b[8] = document.getElementById('b9').value;
 
-		btn.push(document.getElementById("b1"));
-		btn.push(document.getElementById("b2"));
-		btn.push(document.getElementById("b3"));
-		btn.push(document.getElementById("b4"));
-		btn.push(document.getElementById("b5"));
-		btn.push(document.getElementById("b6"));
-		btn.push(document.getElementById("b7"));
-		btn.push(document.getElementById("b8"));
-		btn.push(document.getElementById("b9"));
+	btn.push(document.getElementById("b1"));
+	btn.push(document.getElementById("b2"));
+	btn.push(document.getElementById("b3"));
+	btn.push(document.getElementById("b4"));
+	btn.push(document.getElementById("b5"));
+	btn.push(document.getElementById("b6"));
+	btn.push(document.getElementById("b7"));
+	btn.push(document.getElementById("b8"));
+	btn.push(document.getElementById("b9"));
 
 
-		if (!victory) {
-			checkForWin([b[0], b[1], b[2]], [btn[0], btn[1], btn[2]]);
-		}
-		if (!victory) {
-			checkForWin([b[3], b[4], b[5]], [btn[3], btn[4], btn[5]]);
-		}
-		if (!victory) {
-			checkForWin([b[6], b[7], b[8]], [btn[6], btn[7], btn[8]]);
-		}
-		if (!victory) {
-			checkForWin([b[0], b[3], b[6]], [btn[0], btn[3], btn[6]]);
-		}
-		if (!victory) {
-			checkForWin([b[1], b[4], b[7]], [btn[1], btn[4], btn[7]]);
-		}
-		if (!victory) {
-			checkForWin([b[2], b[5], b[8]], [btn[2], btn[5], btn[8]]);
-		}
-		if (!victory) {
-			checkForWin([b[0], b[4], b[8]], [btn[0], btn[4], btn[8]]);
-		}
-		if (!victory) {
-			checkForWin([b[2], b[4], b[6]], [btn[2], btn[4], btn[6]]);
-		}
-		if (victory) {
-			for (let x of btn) {
-				x.disabled = true;
-			}
-		}
-
-		if (!victory) {
-			// Here, Printing Result
-			if (flag === 1) {
-				document.getElementById('print')
-					.innerHTML = "Player X Turn";
-			}
-			else {
-				document.getElementById('print')
-					.innerHTML = "Player 0 Turn";
-			}
+	if (!victory) {
+		checkForWin([b[0], b[1], b[2]], [btn[0], btn[1], btn[2]]);
+	}
+	if (!victory) {
+		checkForWin([b[3], b[4], b[5]], [btn[3], btn[4], btn[5]]);
+	}
+	if (!victory) {
+		checkForWin([b[6], b[7], b[8]], [btn[6], btn[7], btn[8]]);
+	}
+	if (!victory) {
+		checkForWin([b[0], b[3], b[6]], [btn[0], btn[3], btn[6]]);
+	}
+	if (!victory) {
+		checkForWin([b[1], b[4], b[7]], [btn[1], btn[4], btn[7]]);
+	}
+	if (!victory) {
+		checkForWin([b[2], b[5], b[8]], [btn[2], btn[5], btn[8]]);
+	}
+	if (!victory) {
+		checkForWin([b[0], b[4], b[8]], [btn[0], btn[4], btn[8]]);
+	}
+	if (!victory) {
+		checkForWin([b[2], b[4], b[6]], [btn[2], btn[4], btn[6]]);
+	}
+	if (victory) {
+		for (let x of btn) {
+			x.disabled = true;
 		}
 	}
+
+	if (!victory) {
+		if (turn >= 8) {
+			//Tie if all boxes are full
+			document.getElementById('print').innerHTML = "Match Tie";
+		}
+		// Here, Printing Result
+		else if (flag === 1) {
+			document.getElementById('print')
+				.innerHTML = "Player X Turn";
+		}
+		else {
+			document.getElementById('print')
+				.innerHTML = "Player 0 Turn";
+		}
+	}
+	turn++;
 }
+
 
 // Function to reset game
 function resetBoard() {
